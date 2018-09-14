e3241148f4fe34b654a368e2cdbf444244746d4f
directman66 <sannikovdi@yandex.ru>
2018-09-14 16:47:30 +0500

    Update indata_edit.inc.php

--- modules/mercury/indata_edit.inc.php
+++ modules/mercury/indata_edit.inc.php
@@ -7,75 +7,66 @@
   }
   $table_name='mercury_devices';
   $rec=SQLSelectOne("SELECT * FROM $table_name WHERE ID='$id'");
+
+//sg('test.merk',$this->mode);
   if ($this->mode=='update') {
    $ok=1;
   
  
 	 global $title;
+
    $rec['TITLE']=$title;
-   if ($rec['TITLE']=='') {
-    $out['ERR_TITLE']=1;
-    $ok=0;
-   }
+sg('test.merktitle',$title);
+
   
 
 	 global $port;
    $rec['PORT']=$port;
+/*
    if ($rec['PORT']=='') {
     $out['ERR_PORT']=1;
     $ok=0;
    }
+*/
 
 	 global $hexadr;
    $rec['HEXADR']=$hexadr;
+/*
    if ($rec['HEXADR']=='') {
     $out['ERR_HEXADR']=1;
     $ok=0;
    }
+*/
 
 
 	 global $ipaddr;
    $rec['IPADDR']=$ipaddr;
+/*
    if ($rec['IPADDR']=='') {
     $out['ERR_IPADDR']=1;
     $ok=0;
    }
+*/
 
 	 global $model;
    $rec['MODEL']=$model;
-   if ($rec['MODEL']=='') {
-    $out['ERR_MODEL']=1;
-    $ok=0;
-   }
 
 	 global $fio;
    $rec['FIO']=$fio;
-   if ($rec['FIO']=='') {
-    $out['ERR_FIO']=1;
-    $ok=0;
-   }
+
+
+
 
 	 global $phone;
    $rec['PHONE']=$phone;
-   if ($rec['PHONE']=='') {
-    $out['ERR_PHONE']=1;
-    $ok=0;
-   }
+
 	 global $street;
+
    $rec['STREET']=$street;
-   if ($rec['STREET']=='') {
-    $out['ERR_STREET']=1;
-    $ok=0;
-   }
 
-
-   global $linked_object;
-   $rec['LINKED_OBJECT']=$linked_object;
-  
-   global $linked_property;
-   $rec['LINKED_PROPERTY']=$linked_property;
    
    //UPDATING RECORD
+//sg('test.merk',$ok);
    if ($ok) {
     if ($rec['ID']) {
      SQLUpdate($table_name, $rec); // update
@@ -97,4 +88,3 @@
    }
   }
   outHash($rec, $out);
-
