9941fee5409f95079bcee300fea026c93c230e05
DMITRIY SANNIKOV <sannikovdi@yandex.ru>
2018-09-21 00:15:38 +0500

    Update check.php

--- modules/mercury/lk/check.php
+++ modules/mercury/lk/check.php
@@ -3,14 +3,12 @@
 
 // Соединямся с БД
 
-
 //$link=mysqli_connect("localhost", EXT_ACCESS_USERNAME, "EXT_ACCESS_PASSWORD", "testtable");
 //echo 'coockie_login:'.$_COOKIE['login']."<br>";
 //echo 'coockie_hash:'.$_COOKIE['hash']."<br>";
 //echo 'coockie_id:'.$_COOKIE['id']."<br>";
 //echo '$_SERVER[REMOTE_ADDR]:'.$_SERVER['REMOTE_ADDR']."<br>";
-
-$link=mysqli_connect('localhost', 'pi', 'password', 'db_terminal');
+$link=mysqli_connect('localhost', 'pi', '', 'db_123456');
 //echo "________________";
 
 if (isset($_COOKIE['login']) and isset($_COOKIE['hash']))
@@ -34,7 +32,11 @@ if (isset($_COOKIE['login']) and isset($_COOKIE['hash']))
         print "Доброе время суток! Пользователь:".$userdata['FIO']."(". $userdata['STREET'].")<br><br>";
 
         print "По данным системы мониторинга, потребление вашего счетчика составляет  ".$userdata['PvT'] .' Вт, напряжение на фазах '.$userdata['U']." В, измеренная сила тока ".$userdata['IaT'].' A.<br><br>';
-        print "Регистры счетчика: Тариф 1: ".$userdata['Total1'].", Тариф 2: ".$userdata['Total2'] ;
+        print "Регистры счетчика: Тариф 1: ".$userdata['Total1'].", Тариф 2: ".$userdata['Total2'] .'<br><br>';
+        print "Расходы за день: ".$userdata['DAY_WATT']." Вт /  ".$userdata['DAY_RUB'] .' руб.<br>';
+        print "Расходы за неделю: ".$userdata['WEEK_WATT']." Вт /  ".$userdata['WEEK_RUB'] .'руб.<br>';
+        print "Расходы за месяц: ".$userdata['MONTH_WATT']." Вт /  ".$userdata['MONTH_RUB'] .'руб.<br>';
+        print "Расходы за год: ".$userdata['YEAR_WATT']." Вт /  ".$userdata['YEAR_RUB'] .'руб.<br>';
 
     }
 }
