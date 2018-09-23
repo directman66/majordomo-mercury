ed860d0ba04400b010c8e03c8f88c4475e3499f3
DMITRIY SANNIKOV <sannikovdi@yandex.ru>
2018-09-24 00:53:11 +0500

    Update check.php

--- modules/mercury/lk/check.php
+++ modules/mercury/lk/check.php
@@ -1,20 +1,29 @@
 <?
 // Скрипт проверки
 
-// Соединямся с БД
 
-//$link=mysqli_connect("localhost", EXT_ACCESS_USERNAME, "EXT_ACCESS_PASSWORD", "testtable");
-//echo 'coockie_login:'.$_COOKIE['login']."<br>";
-//echo 'coockie_hash:'.$_COOKIE['hash']."<br>";
-//echo 'coockie_id:'.$_COOKIE['id']."<br>";
-//echo '$_SERVER[REMOTE_ADDR]:'.$_SERVER['REMOTE_ADDR']."<br>";
-$link=mysqli_connect('localhost', 'pi', '', 'db_123456');
+ chdir('../');
+ include_once("./config.php");
+ include_once("./lib/loader.php");
+ include_once(DIR_MODULES."application.class.php");
+ $db = new mysql(DB_HOST, '', DB_USER, DB_PASSWORD, DB_NAME); //connecting to database
+ include_once("./load_settings.php");
+
+//---------------------------- MySQL
+
+//$db=new mysql(DB_HOST, '', DB_USER, DB_PASSWORD, DB_NAME);
+
+//$link=mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+//$link=new mysql(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+//$db=new mysql(DB_HOST, '', DB_USER, DB_PASSWORD, DB_NAME);
 //echo "________________";
 
 if (isset($_COOKIE['login']) and isset($_COOKIE['hash']))
 {
-    $query = mysqli_query($link, "SELECT * FROM mercury_devices WHERE LOGIN = '".$_COOKIE['login']."' LIMIT 1");
-    $userdata = mysqli_fetch_assoc($query);
+$sql="SELECT * FROM mercury_devices WHERE LOGIN = '".$_COOKIE['login']."'";
+$userdata=SQLSelectOne($sql);
+//    $query = mysqli_query($link, "SELECT * FROM mercury_devices WHERE LOGIN = '".$_COOKIE['login']."' LIMIT 1");
+//    $userdata = mysqli_fetch_assoc($query);
 //echo $userdata['USERHASH'].'<br>';
 //echo $userdata['LOGIN'].'<br>';
 //echo $userdata['USERIP'].'<br>';
@@ -38,10 +47,185 @@ if (isset($_COOKIE['login']) and isset($_COOKIE['hash']))
         print "Расходы за месяц: ".$userdata['MONTH_WATT']." Вт /  ".$userdata['MONTH_RUB'] .'руб.<br>';
         print "Расходы за год: ".$userdata['YEAR_WATT']." Вт /  ".$userdata['YEAR_RUB'] .'руб.<br>';
 
+
+
+
     }
+
 }
 else
 {
     print "Включите куки";
 }
+
 ?>
+
+<div id="chart1" style="height: 300px"></div>
+<script src="https://code.highcharts.com/highcharts.js"></script>
+
+
+<script type="text/javascript" name="1">
+Highcharts.chart('chart1', {
+
+  chart: {
+    borderWidth: 0,
+    plotBorderWidth: 1,
+    spacingTop: 10
+    ,width: 350 // 16:9 ratio
+
+  },
+
+title: {
+            text: 'Сила тока:'
+        },
+
+
+
+  xAxis: {
+    categories: [
+<? 
+$sql="SELECT left(ADDED,10) dt, round(AVG(phistory.value),2) value FROM objects, pvalues,phistory where objects.ID=pvalues.OBJECT_ID and pvalues.PROPERTY_NAME='Mercury_".$userdata['ID'].".IaT' and phistory.VALUE_ID=pvalues.ID group by left(ADDED,10)";
+$cmd_rec = SQLSelect($sql);
+$stroka="";
+foreach ($cmd_rec as $cmd_r)
+{$stroka.= '"'.$cmd_r['dt'].'",';
+}
+$stroka=preg_replace("/(.)$/", "", $stroka);
+echo $stroka;
+
+?>
+
+
+]
+  },
+
+  series: [{
+    data: [
+<? 
+$sql="SELECT left(ADDED,10) dt, round(AVG(phistory.value),2) value FROM objects, pvalues,phistory where objects.ID=pvalues.OBJECT_ID and pvalues.PROPERTY_NAME='Mercury_".$userdata['ID'].".IaT' and phistory.VALUE_ID=pvalues.ID group by left(ADDED,10)";
+$cmd_rec = SQLSelect($sql);
+$stroka="";
+foreach ($cmd_rec as $cmd_r)
+{$stroka.= $cmd_r['value'].",";
+}
+$stroka=preg_replace("/(.)$/", "", $stroka);
+echo $stroka;
+
+?>
+]
+  }]
+
+});
+</script>
+
+
+<script src="https://code.highcharts.com/highcharts.js" name="123"></script>
+<div id="chart2" style="height: 300px"></div>
+
+		<script type="text/javascript" name="123">
+Highcharts.chart('chart2', {
+
+  chart: {
+    borderWidth: 0,
+    plotBorderWidth: 0,
+    spacingTop: 10
+    ,width: 350 // 16:9 ratio
+  },
+title: {
+            text: 'Напряжение:'
+        },
+
+  xAxis: {
+    categories: [
+<? 
+$sql="SELECT left(ADDED,10) dt, round(AVG(phistory.value),2) value FROM objects, pvalues,phistory where objects.ID=pvalues.OBJECT_ID and pvalues.PROPERTY_NAME='Mercury_".$userdata['ID'].".U' and phistory.VALUE_ID=pvalues.ID group by left(ADDED,10)";
+$cmd_rec = SQLSelect($sql);
+$stroka="";
+foreach ($cmd_rec as $cmd_r)
+{$stroka.= '"'.$cmd_r['dt'].'",';
+}
+$stroka=preg_replace("/(.)$/", "", $stroka);
+echo $stroka;
+
+?>
+
+
+]
+  },
+
+  series: [{
+    data: [
+<? 
+$sql="SELECT left(ADDED,10) dt, round(AVG(phistory.value),2) value FROM objects, pvalues,phistory where objects.ID=pvalues.OBJECT_ID and pvalues.PROPERTY_NAME='Mercury_".$userdata['ID'].".U' and phistory.VALUE_ID=pvalues.ID group by left(ADDED,10)";
+$cmd_rec = SQLSelect($sql);
+$stroka="";
+foreach ($cmd_rec as $cmd_r)
+{$stroka.= $cmd_r['value'].",";
+}
+$stroka=preg_replace("/(.)$/", "", $stroka);
+echo $stroka;
+
+?>
+]
+  }]
+
+});
+		</script>
+
+
+<script src="https://code.highcharts.com/highcharts.js" name="123"></script>
+<div id="chart3" style="height: 300px"></div>
+
+		<script type="text/javascript" name="123">
+Highcharts.chart('chart3', {
+
+  chart: {
+    borderWidth: 1,
+    plotBorderWidth: 0,
+    spacingTop: 10
+    ,width: 350
+
+  },
+title: {
+            text: 'Потребляемая мощность:'
+        },
+
+  xAxis: {
+    categories: [
+<? 
+$sql="SELECT left(ADDED,10) dt, round(AVG(phistory.value),2) value FROM objects, pvalues,phistory where objects.ID=pvalues.OBJECT_ID and pvalues.PROPERTY_NAME='Mercury_".$userdata['ID'].".PvT' and phistory.VALUE_ID=pvalues.ID group by left(ADDED,10)";
+$cmd_rec = SQLSelect($sql);
+$stroka="";
+foreach ($cmd_rec as $cmd_r)
+{$stroka.= '"'.$cmd_r['dt'].'",';
+}
+$stroka=preg_replace("/(.)$/", "", $stroka);
+echo $stroka;
+
+?>
+
+
+]
+  },
+
+  series: [{
+    data: [
+<? 
+$sql="SELECT left(ADDED,10) dt, round(AVG(phistory.value),2) value FROM objects, pvalues,phistory where objects.ID=pvalues.OBJECT_ID and pvalues.PROPERTY_NAME='Mercury_".$userdata['ID'].".PvT' and phistory.VALUE_ID=pvalues.ID group by left(ADDED,10)";
+$cmd_rec = SQLSelect($sql);
+$stroka="";
+foreach ($cmd_rec as $cmd_r)
+{$stroka.= $cmd_r['value'].",";
+}
+$stroka=preg_replace("/(.)$/", "", $stroka);
+echo $stroka;
+
+?>
+]
+  }]
+
+});
+		</script>
+
+
+
