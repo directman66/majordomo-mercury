ab4b1398f8d8aed3ac4d2c0f9cd9e160a3c9441f
DMITRIY SANNIKOV <sannikovdi@yandex.ru>
2019-01-28 19:44:24 +0500

    Update check.php

--- modules/mercury/lk/check.php
+++ modules/mercury/lk/check.php
@@ -4,11 +4,15 @@
 
 
 chdir(dirname(__FILE__) . '/../../..');
+//chdir(dirname(__FILE__) . '/../..');
  include_once("./config.php");
- include_once("./lib/loader.php");
- include_once(DIR_MODULES."application.class.php");
- $db = new mysql(DB_HOST, '', DB_USER, DB_PASSWORD, DB_NAME); //connecting to database
- include_once("./load_settings.php");
+// include_once("./lib/loader.php");
+// include_once(DIR_MODULES."application.class.php");
+// $db = new mysql(DB_HOST, '', DB_USER, DB_PASSWORD, DB_NAME); //connecting to database
+
+$db=mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+// include_once("./load_settings.php");
 
 //---------------------------- MySQL
 
@@ -22,7 +26,12 @@ chdir(dirname(__FILE__) . '/../../..');
 if (isset($_COOKIE['login']) and isset($_COOKIE['hash']))
 {
 $sql="SELECT * FROM mercury_devices WHERE LOGIN = '".$_COOKIE['login']."'";
-$userdata=SQLSelectOne($sql);
+//$userdata=SQLSelectOne($sql);
+
+	$query= mysqli_query($db,$sql);
+    	$userdata = mysqli_fetch_assoc($query);
+
+
 //    $query = mysqli_query($link, "SELECT * FROM mercury_devices WHERE LOGIN = '".$_COOKIE['login']."' LIMIT 1");
 //    $userdata = mysqli_fetch_assoc($query);
 //echo $userdata['USERHASH'].'<br>';
@@ -42,14 +51,16 @@ $userdata=SQLSelectOne($sql);
 
 echo DIR_MODULES.'mercury/lk3';
 if (!$userdata['PREDSED']=='1') {
-		  require(DIR_MODULES.'mercury/lk3/user.php');
+//		  require(DIR_MODULES.'mercury/lk3/user.php');
+		  require('./cms/lk/user.php');
 		
 				}
 
 		else 
 				{
 
-		  require(DIR_MODULES.'mercury/lk3/predsed.php');
+//		  require(DIR_MODULES.'mercury/lk3/predsed.php');
+		  require('./cms/lk/predsed.php');
 
 			       }
 
