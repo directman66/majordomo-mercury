7045da7f70cf35d28d0d63e290cd8d81274ed717
DMITRIY SANNIKOV <sannikovdi@yandex.ru>
2019-01-28 19:58:53 +0500

    Update deletenews.php

--- modules/mercury/deletenews.php
+++ modules/mercury/deletenews.php
@@ -2,13 +2,13 @@
 
 chdir(dirname(__FILE__) . '/../..');
 include_once("./config.php");
-include_once("./lib/loader.php");
-include_once("./modules/application.class.php");
-include_once("./load_settings.php");
+//include_once("./lib/loader.php");
+//include_once("./modules/application.class.php");
+//include_once("./load_settings.php");
 
 
 //debmes('Удаление новости '.$_POST['id'], 'mercury');
-debmes('Удаление новости '.$_GET['id'], 'mercury');
+//debmes('Удаление новости '.$_GET['id'], 'mercury');
 
 
 
@@ -17,9 +17,14 @@ $link=mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
 if(isset($_GET['id']))
 {
+//$sql="delete from  mercury_news where id=".$_GET['id'];
+//debmes($sql, 'mercury');
+//SQLExec($sql);
+
 $sql="delete from  mercury_news where id=".$_GET['id'];
-debmes($sql, 'mercury');
-SQLExec($sql);
+$mysqli= mysqli_query($link,$sql);
+
+
 
 }
 
