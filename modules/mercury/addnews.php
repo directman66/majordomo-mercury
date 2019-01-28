5654584035afe3c9dac23095df1de6a7f918464d
DMITRIY SANNIKOV <sannikovdi@yandex.ru>
2019-01-28 14:45:48 +0500

    Create addnews.php

---
+++ modules/mercury/addnews.php
@@ -0,0 +1,30 @@
+<?
+
+chdir(dirname(__FILE__) . '/../..');
+include_once("./config.php");
+include_once("./lib/loader.php");
+include_once("./modules/application.class.php");
+include_once("./load_settings.php");
+
+
+debmes('Пришла новая новость', 'mercury');
+
+$link=mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+
+if(isset($_POST['addnews']))
+{
+$tema=$_REQUEST['tema'];
+$message=$_REQUEST['message'];
+debmes('Добавление новой новости '.$news, 'mercury');
+
+
+$news = SQLSelect("SELECT * FROM mercury_news");
+{
+$news['data']=date('Y-m-d H:i:s');;
+$news['message']=$tema;
+$news['TITLE']=$message;
+SQLInsert('mercury_news', $news);	
+ }
+
+}
